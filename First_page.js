af949ffc541581e6df647bd3ada6d392c5628289
Sumit524 <125038496+Sumit524@users.noreply.github.com>
2023-09-16 15:47:27 +0530

    Update First_page.js

--- First_page.js
+++ First_page.js
@@ -1,10 +1,10 @@
 // Select all elements with class .small-box
 const smallBoxes = document.querySelectorAll('.thirdcontainer .small-box');
 
-// Select the special box
+
 const specialBox = document.querySelector('.thirdcontainer .small-box_special');
 
-// Function to generate a random color in hexadecimal format
+
 function getRandomColor() {
   
   for (let i = 0; i < 6; i++) {
@@ -13,23 +13,23 @@ function getRandomColor() {
   return color;
 }
 
-// Function to change the background color of a box
+
 function changeBoxColor(box) {
   box.style.backgroundColor = getRandomColor();
 }
 
-// Add a click event listener to the special box
+
 specialBox.addEventListener('click', () => {
-  // Change the color of all small boxes when the special box is clicked
+
   smallBoxes.forEach(box => {
     changeBoxColor(box);
   });
 });
 
-// Add a click event listener to each small box (except the special box)
+
 smallBoxes.forEach(box => {
   box.addEventListener('click', () => {
-    // Change the color of the clicked box
+    
     changeBoxColor(box);
   });
 });
